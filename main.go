765a6aa0a26878e84117e5b1ed6b2c272124a215
Tsubasa Takayama <t-takayama@feedforce.jp>
2018-04-25 14:25:55 +0900

    Implement DogStatsd tutorial
    ref: https://github.com/DataDog/datadog-go/tree/e67964b4021ad3a334e748e8811eb3cd6becbc6e#statsd

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/DataDog/datadog-go/statsd"
+)
+
+func main() {
+	c, err := statsd.New("127.0.0.1:8125")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Namespace = "flubber."
+	c.Tags = append(c.Tags, "region:us-east-1a")
+
+	if err = c.Gauge("request.duration", 1.2, nil, 1); err != nil {
+		log.Fatal(err)
+	}
+}
